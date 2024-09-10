bd4f25e64ae12e0ec34a67dfe393c84ab5e73008
mouskit2 <mtraore@live.fr>
2024-09-10 16:46:57 +0200

    Version gamification de la page navigation et de extension

--- script.js
+++ script.js
@@ -38,17 +38,6 @@ function addTask() {
   saveData();
 }
 
-btnLeft.addEventListener("click", () => {
-  chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
-    chrome.scripting.executeScript({
-      target: {
-        tabId: tabs[0].id,
-      },
-      function: makePageBlack,
-    });
-  });
-});
-
 function endEvent() { 
   todoApp.style.display = 'block';
   eventTask.style.display = 'none';
@@ -59,26 +48,97 @@ btnRight.addEventListener("click", function (e) {
   endEvent()
 });
 
-function makePageBlack() {
-  document.body.style.backgroundColor = "black";
-}
 function taskEvent(){
   console.log("taskEvent running");
   todoApp.style.display = 'none';
   eventTask.style.display = 'block';
+  //playGameEvent();
 
-  } 
+  }
 
-function playGameEvent() { 
-   chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+function playGame() {
+  function addStyles(element,styles){
+    for(let id in styles){
+      element.style[id] = styles[id];
+    }
+  }
+  let gameDiv = document.createElement('div');
+  gameDiv.innerHTML = `
+  <div id="lien">
+      <a id="collect" href="http://www.google.com" target="_blank">LIEN GOOGLE</a>
+  </div>
+  <div id="filtre-noir"></div>
+  <!-- <div class="cursor"></div> -->
+  <script src="/Test CSS/script-test.js"></script>
+  `;
+  document.body.appendChild(gameDiv);
+
+  let nFilter = document.getElementById('filtre-noir');
+  let styles = {
+    position: "absolute",
+    width: "200%",
+    height: "200vh",
+    backgroundColor: "black",
+    pointerEvents: "none",
+    top: "0px",
+    left:"0px",
+    zindex:"995",
+    transform: "translateX(-50%) translateY(-50%)",
+    maskPosition: "2px 23px",
+    maskOrigin: "pointer",
+    maskImage: "radial-gradient(circle, transparent 2%, black 5%)"
+  }
+  addStyles(nFilter,styles);
+
+  let collect = document.getElementById('collect');
+  let a_style = {
+    position: "relative"
+  }
+  addStyles(collect,a_style);
+
+  const bodyFiltre = document.querySelector('body')
+  bodyStyle = {
+    /* z-index: 2; */
+    backgroundcolor: "yellow",
+    display: "flex",
+    position: "relative"
+  }
+  addStyles(bodyFiltre,bodyStyle);
+
+  let lien = document.getElementById('lien');
+  let lien_style = {
+    position: "absolute",
+    top: "0px",
+    left: "0px",
+    zindex:"999"
+  }
+  addStyles(lien,lien_style);
+
+
+  function moveMask() {
+
+    document.addEventListener('mousemove', function (m) {
+        // let position = filtre.getBoundingClientRect();
+
+        nFilter.style["mask-position"] = m.clientX + 'px ' + m.clientY + 'px'
+
+    })
+  };
+  moveMask()
+}
+
+btnLeft.addEventListener("click", () => {
+  todoApp.style.display = 'block';
+  eventTask.style.display = 'none';
+  chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
     chrome.scripting.executeScript({
       target: {
         tabId: tabs[0].id,
       },
-      function: makePageBlack,
+      function: playGame,
     });
   });
-}
+});
 
 listContainer.addEventListener(
   "click",
